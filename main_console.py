fcf83d4a2f996ecc897e7d18c005271d8e65b74b
Nastia Yemelianova <49037481+discoDeaky@users.noreply.github.com>
2020-03-07 15:10:55 +0200

    Add files via upload

--- main_console.py
+++ main_console.py
@@ -1,35 +1,9 @@
 from random import shuffle
 
 
-def inputt():
-    spisok = []
-    num = []
-    c1 = int(input('Enter first position: '))
-    if len(str(c1)) > 3 or str(c1).isalpha():
-        print('Error.Enter a correct number')
-        inputt()
-    king(c1,steps)
-    spisok.append(cards[c1])
-    c2 = int(input('Enter second position: '))
-    if len(str(c2)) > 3 or str(c2).isalpha():
-        print('Error.Enter a correct number')
-        inputt()
-    spisok.append(cards[c2])
-    Desk.input_card(Desk, num, spisok, c1, c2)
-    return spisok
-
-
-def king(c1, steps):
-    if "K" in cards[c1]:
-        cards[c1] = '**'
-        steps += 1
-        Cards.view(Cards, cards, stock,steps)
-        inputt()
-
-
 class Desk:
 
-    def __init__(self, spisok, num, cards, stock, steps=0):
+    def __init__(self, spisok, num, cards, stock, steps):
         self.spisok = spisok
         self.num = num
         self.cards = cards
@@ -37,7 +11,6 @@ class Desk:
         self.steps = steps
 
     def input_card(self, num, spisok, c1, c2):
-
         for i in range(len(spisok)):
             if "A" in spisok[i]:
                 num.append(1)
@@ -65,23 +38,23 @@ class Desk:
                 num.append(3)
             if '2' in spisok[i]:
                 num.append(2)
-        Desk.summ(Desk, num, spisok, c1, c2, steps)
+        Desk.summ(Desk, num, spisok, c1, c2)
 
-    def summ(self, num, spisok, c1, c2, steps):
+    def summ(self, num, spisok, c1, c2):
         if sum(num) == 13:
             cards[c1] = '**'
             cards[c2] = '**'
-            steps += 1
             spisok.clear()
-            Cards.view(Cards, cards, stock, steps)
+            Cards.view(Cards, cards, stock)
             inputt()
         else:
-            Cards.view(Cards, cards, stock, steps)
+            Cards.view(Cards, cards, stock)
             inputt()
+
 
 class Cards(Desk):
 
-    def view(self, cards, stock, steps):
+    def view(self, cards, stock):
         print(f'              {cards[0]}\n'
               f'            {cards[1]}  {cards[2]}\n'
               f'          {cards[3]}  {cards[4]}  {cards[5]}\n'
@@ -89,16 +62,40 @@ class Cards(Desk):
               f'      {cards[10]}  {cards[11]}  {cards[12]}  {cards[13]}  {cards[14]}\n'
               f'    {cards[15]}  {cards[16]}  {cards[17]}  {cards[18]}  {cards[19]}  {cards[20]}\n'
               f'  {cards[21]}  {cards[22]}  {cards[23]}  {cards[24]}  {cards[25]}  {cards[26]}  {cards[27]}\n'
-              f'STOCK: {stock[0]}                              SCORE: {steps}')
+              f'STOCK: {stock[0]}                              SCORE: {"-"}')
+
+
+def inputt():
+    spisok = []
+    num = []
+    c1 = int(input('Enter first position: '))
+    if c1 >= 28 or str(c1).isalpha():
+        print('Error.Enter a correct number')
+        inputt()
+    king(c1)
+    spisok.append(cards[c1])
+    c2 = int(input('Enter second position: '))
+    if c2 >= 28 or str(c2).isalpha():
+        print('Error.Enter a correct number')
+        inputt()
+    spisok.append(cards[c2])
+    Desk.input_card(Desk, num, spisok, c1, c2)
+    return spisok
+
+
+def king(c1):
+    if "K" in cards[c1]:
+        cards[c1] = '**'
+        Cards.view(Cards, cards, stock)
+        inputt()
 
 
 if __name__ == "__main__":
-    steps = 0
     cards = ["AH", "KH", "QH", "JH", "10H", "9H", "8H", "7H", "6H", "5H", "4H", "3H", "2H",
              "AP", "KP", "QP", "JP", "10P", "9P", "8P", "7P", "6P", "5P", "4P", "3P", "2P",
              "AC", "KC", "QC", "JC", "10C", "9C", "8C", "7C", "6C", "5C", "4C", "3C", "2C",
              "AT", "KT", "QT", "JT", "10T", "9T", "8T", "7T", "6T", "5T", "4T", "3T", "2T"]
     shuffle(cards)
     stock = cards[28:]
-    Cards.view(Cards, cards, stock, steps)
+    Cards.view(Cards, cards, stock)
     inputt()
