087d61e88fa110e09ea8dd2990a5aae64292f8cd
Anastasiia Yemelianova <49037481+discoDeaky@users.noreply.github.com>
2020-04-10 18:03:53 +0300

    Add files via upload

--- main_desktop.py
+++ main_desktop.py
@@ -7,10 +7,15 @@ from PyQt5.QtWidgets import *
 from random import shuffle
 from functools import partial
 
-
 _translate = QtCore.QCoreApplication.translate
 
-class Desk:
+
+class Cards:
+
+    # def __init__(self, index=0):
+    #     super(Desk, self).__init__()
+    #     self.index = index
+
     def value(self, pushButton, num):
         if pushButton.isEnabled():
             if "A" in pushButton.text():
@@ -52,10 +57,9 @@ class Desk:
             if "2" in pushButton.text():
                 num.append(2)
                 names.append(pushButton)
-            Desk.summ(Desk, num, names, pushButton, stock)
+            Desk.summ(Desk, num, names, stock)
 
-    def summ(self, num, names, pushButton, stock):
-
+    def summ(self, num, names, stock):
         if len(names) == 2 and sum(num) != 13:
             num.clear()
             names.clear()
@@ -65,95 +69,123 @@ class Desk:
                     temp = stock.index(names[0].text())
                     del stock[stock.index(names[0].text())]
                     ui.pushButton_30.setText(stock[temp])
+                    ui.pushButton_30.setIcon(QIcon(QPixmap(f"PNG/{stock[temp]}.png")))
+                    num.clear()
+                    names.clear()
+                    score()
                 else:
                     names[0].hide()
                     num.clear()
                     names.clear()
+                    score()
                     Desk.all_cards_access(Desk, pyramid)
             elif len(names) == 2:
+                # if names[0] == ui.pushButton_30 and names[1] == ui.pushButton_30:
+                #     num.clear()
+                #     names.clear()
                 if names[0] == ui.pushButton_30:
                     temp1 = stock.index(names[0].text())
                     del stock[stock.index(names[0].text())]
                     ui.pushButton_30.setText(stock[temp1])
+                    ui.pushButton_30.setIcon(QIcon(QPixmap(f"PNG/{stock[temp1]}.png")))
                     names[1].hide()
                     num.clear()
                     names.clear()
+                    score()
                     Desk.all_cards_access(Desk, pyramid)
                 elif names[1] == ui.pushButton_30:
                     temp2 = stock.index(names[1].text())
                     del stock[stock.index(names[1].text())]
                     ui.pushButton_30.setText(stock[temp2])
+                    ui.pushButton_30.setIcon(QIcon(QPixmap(f"PNG/{stock[temp2]}.png")))
                     names[0].hide()
                     num.clear()
                     names.clear()
+                    score()
                     Desk.all_cards_access(Desk, pyramid)
                 else:
                     names[0].hide()
                     names[1].hide()
                     num.clear()
                     names.clear()
+                    score()
                     Desk.all_cards_access(Desk, pyramid)
 
     def all_cards_access(self, pyramid):
         row = 1
         for i in range(len(pyramid[:21])):
-            if not pyramid[i].isEnabled():
-                if i == 1:
-                    row += 1
-                if i == 3:
-                    row += 1
-                if i == 6:
-                    row += 1
-                if i == 10:
-                    row += 1
-                if i == 15:
-                    row += 1
-                if i == 21:
-                    row += 1
-                if pyramid[i + row].isHidden() and pyramid[i + row + 1].isHidden():
-                    pyramid[i].setEnabled(True)
-                    pyramid[i].setIcon(QIcon(None))
-                    pyramid[i].setText(_translate("MainWindow", cards[i]))
-
-    # def score(self, pushButton):
-    #     if Desk.value(Desk, pushButton) == 13:
-    #         pass
+            if i == 1:
+                row += 1
+            if i == 3:
+                row += 1
+            if i == 6:
+                row += 1
+            if i == 10:
+                row += 1
+            if i == 15:
+                row += 1
+            if i == 21:
+                row += 1
+            if pyramid[i + row].isHidden() and pyramid[i + row + 1].isHidden():
+                pyramid[i].setEnabled(True)
+                pyramid[i].setIcon(QIcon(None))
+                pyramid[i].setText(_translate("MainWindow", cards[i]))
 
 
-class Cards(Desk):
+class Desk(Cards):
+
+    # def __init__(self):
+    #     super().__init__()
     def __init__(self, index=0):
         self.index = index
 
     def iter_stock(self):
+        d.stock_style()
         if self.index == len(stock):
             self.index = 0
-        stock_style()
         ui.pushButton_30.setText(_translate("MainWindow", stock[self.index]))
+        ui.pushButton_30.setIcon(QIcon(QPixmap(f"PNG/{stock[self.index]}.png")))
         self.index += 1
+        score()
 
-def not_enabled_pyramid():
-    # icon = QIcon()
-    # icon.addPixmap(QPixmap("red_back.png"), QIcon.Active, QIcon.Off)
-    for i in range(21):
-        pyramid[i].setText(_translate("MainWindow", None))
-        pyramid[i].setEnabled(False)
-        pyramid[i].setIconSize(QSize(125, 125))
-        pyramid[i].setIcon(QIcon("PNG/red_back.png"))
-        # pyramid[i].setIcon(icon)
+    def stock_style(self):
+        ui.pushButton_29.setText(_translate("MainWindow", None))
+        ui.pushButton_29.setIcon(QIcon(QPixmap("PNG/red_back.png")))
+        ui.pushButton_30.setText(_translate("MainWindow", None))
+        ui.pushButton_29.setIconSize(QSize(125, 125))
+        ui.pushButton_30.setIconSize(QSize(125, 125))
 
-        # icon.addPixmap(QPixmap(graphics_dir+name+".png"), QIcon.Normal, QIcon.Off)
+    def not_enabled_pyramid(self):
+        for i in range(21):
+            pyramid[i].setText(_translate("MainWindow", None))
+            pyramid[i].setEnabled(False)
+            pyramid[i].setIconSize(QSize(125, 125))
+            pyramid[i].setIcon(QIcon(QPixmap("PNG/red_back.png")))
 
 
-def stock_style():
-    ui.pushButton_29.setText(_translate("MainWindow", None))
-    ui.pushButton_29.setIcon(QIcon("PNG/red_back.png"))
-    # ui.pushButton_30.setText(_translate("MainWindow", None))
-    ui.pushButton_29.setIconSize(QSize(125, 125))
-    ui.pushButton_30.setIconSize(QSize(125, 125))
+def score():
+    ui.lcdNumber.display(ui.lcdNumber.value()+1)
+    ui.lcdNumber.update()
+
+
+def window_style():
+    ui.setWindowTitle("Pyramid Solitaire")
+    img = QImage("PNG/background.png")
+    palette = QPalette()
+    scaled = img.scaled(ui.size(), Qt.KeepAspectRatioByExpanding)
+    palette.setBrush(QPalette.Window, QBrush(scaled))
+    ui.setPalette(palette)
+    ui.setWindowIcon(QtGui.QIcon(os.path.join('PNG/playing-cards-icon.png')))
+
+
+def new_game():
+    ui.lcdNumber.display(0)
+    ui.lcdNumber.update()
+    ui.lcdNumber.show()
 
 
 def main():
-    ui.pushButton_29.clicked.connect(c.iter_stock)
+    ui.pushButton_29.clicked.connect(d.iter_stock)
     ui.pushButton_1.clicked.connect(partial(Desk.value, Desk, ui.pushButton_1, num))
     ui.pushButton_2.clicked.connect(partial(Desk.value, Desk, ui.pushButton_2, num))
     ui.pushButton_3.clicked.connect(partial(Desk.value, Desk, ui.pushButton_3, num))
@@ -186,30 +218,23 @@ def main():
 
 
 if __name__ == "__main__":
-    c = Cards()
+    d = Desk()
     app = QtWidgets.QApplication([])
-    app.setStyle("Fusion")
-    # palette = QPalette()
-    # palette.setColor(QPalette.Window, QColor(53, 53, 53))
-    # palette.setColor(QPalette.WindowText, Qt.white)
-    # palette.setColor(QPalette.Base, QColor(25, 25, 25))
-    # palette.setColor(QPalette.AlternateBase, QColor(53, 53, 53))
-    # palette.setColor(QPalette.ToolTipBase, Qt.white)
-    # palette.setColor(QPalette.ToolTipText, Qt.white)
-    # palette.setColor(QPalette.Text, Qt.white)
-    # palette.setColor(QPalette.Button, QColor(53, 53, 53))
-    # palette.setColor(QPalette.ButtonText, Qt.white)
-    # palette.setColor(QPalette.BrightText, Qt.red)
-    # palette.setColor(QPalette.Link, QColor(42, 130, 218))
-    # palette.setColor(QPalette.Highlight, QColor(42, 130, 218))
-    # palette.setColor(QPalette.HighlightedText, Qt.black)
-    # app.setPalette(palette)
-    # ui = uic.loadUi(app.theme["ui_path"] + "window.ui")
     ui = uic.loadUi("window.ui")
-    ui.setWindowTitle("Pyramid Solitaire")
-    base_dir = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
-    ui.setWindowIcon(QtGui.QIcon(os.path.join(base_dir, 'PNG/green_felt.jpg')))  # must be in ico format
-    n = QtCore.QPoint(120, 40)
+    window_style()
+    exitActionN = QAction('New game')
+    exitActionN.setShortcut('Ctrl+N')
+    exitActionN.triggered.connect(new_game)
+
+    exitActionE = QAction('Exit')
+    exitActionE.setShortcut('Ctrl+Q')
+    exitActionE.triggered.connect(qApp.quit)
+
+    menubar = ui.menuBar()
+    fileMenu = menubar.addMenu('Menu')
+    fileMenu.addAction(exitActionN)
+    fileMenu.addAction(exitActionE)
+
     cards = ["AH", "KH", "QH", "JH", "10H", "9H", "8H", "7H", "6H", "5H", "4H", "3H", "2H",
              "AD", "KD", "QD", "JD", "10D", "9D", "8D", "7D", "6D", "5D", "4D", "3D", "2D",
              "AC", "KC", "QC", "JC", "10C", "9C", "8C", "7C", "6C", "5C", "4C", "3C", "2C",
@@ -217,16 +242,19 @@ if __name__ == "__main__":
     shuffle(cards)
     pyramid = [ui.pushButton_1, ui.pushButton_2, ui.pushButton_3, ui.pushButton_4, ui.pushButton_5, ui.pushButton_6,
                ui.pushButton_7, ui.pushButton_8, ui.pushButton_9, ui.pushButton_10, ui.pushButton_11, ui.pushButton_12,
-               ui.pushButton_13, ui.pushButton_14, ui.pushButton_15, ui.pushButton_16, ui.pushButton_17, ui.pushButton_18,
-               ui.pushButton_19, ui.pushButton_20, ui.pushButton_21, ui.pushButton_22, ui.pushButton_23, ui.pushButton_24,
+               ui.pushButton_13, ui.pushButton_14, ui.pushButton_15, ui.pushButton_16, ui.pushButton_17,
+               ui.pushButton_18,
+               ui.pushButton_19, ui.pushButton_20, ui.pushButton_21, ui.pushButton_22, ui.pushButton_23,
+               ui.pushButton_24,
                ui.pushButton_25, ui.pushButton_26, ui.pushButton_27, ui.pushButton_28]
     stock = cards[28:]
     num = []
     names = []
     for i in range(28):
         pyramid[i].setText(_translate("MainWindow", cards[i]))
-    not_enabled_pyramid()
-    c.iter_stock()
+    d.not_enabled_pyramid()
+    d.iter_stock()
+    ui.lcdNumber.display(0)
     main()
     ui.show()
     sys.exit(app.exec_())
